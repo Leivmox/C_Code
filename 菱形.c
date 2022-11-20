b4b16fd9edf47c78de7ebf0058eea7aea975c902
Leivzy <118520019+Leivzy@users.noreply.github.com>
2022-11-20 12:24:54 +0800

    菱形

---
+++ 菱形.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+	
+	scanf("%d",&b);
+	while(l<=b);
+	{
+		int l=1;
+		k=b-l;
+		a=2*l-1
+		printf("%*s",k,"%*s",a);
+		++l;
+	}
+	return 0;
+}
+	
+ /*int n=5;//n为空格数量 
+ printf("%*s",n,"");
+ printf("Good");
+ return 0; 8/
+}
