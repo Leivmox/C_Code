4266413b0fcd3f0a2813ab932976f3042c1b3897
Leivzy <leivzy@163.com>
2022-11-20 13:28:49 +0800

    第一次上传更改

---
+++ Project1/Project1/10.15.1(try).cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+	printf("%d", a);
+	getchar();
+
+	return 0;
+
+}
---
+++ Project1/Project1/10.16(1).c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() {
+
+	int num = 4;
+	printf("%d\n", num);
+
+	return 0;
+
+}
